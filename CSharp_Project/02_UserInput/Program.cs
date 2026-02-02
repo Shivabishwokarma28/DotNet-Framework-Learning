854f3bdea7a694accce412a5d9c5289fc6701774
shivabishwokarma28 <shivabishwokarma90@gmail.com>
2026-02-02 19:33:45 +0545

    Add 02_UserInput project to CSharp_Project

---
+++ CSharp_Project/02_UserInput/Program.cs
@@ -0,0 +1,28 @@
+﻿// Simple user input program
+using System;
+namespace UserInput
+{
+    class Student
+    {
+        string name;
+        int age;
+        void getdata()
+        {
+             Console.WriteLine("Enter your name:");
+            name=Console.ReadLine();//UI
+            Console.WriteLine("Enter your age:");
+            age=Convert.ToInt32(Console.ReadLine());
+        }
+        void print()
+        {
+            Console.WriteLine($"The name of student is {name}");
+            Console.WriteLine($"The age of student is {age}");
+        }
+        public static void Main(string [] args)
+        {
+           Student s1=new Student();
+              s1.getdata();
+                s1.print();
+        }
+    }
+}
