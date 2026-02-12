860849022f783f5970b9ef4a7ce8108938121d71
shivabishwokarma28 <shivabishwokarma90@gmail.com>
2026-02-12 22:17:17 +0545

    Initial commit for JWT ASP.NET Core API

--- CSharp_Project/02_UserInput/obj/Debug/net8.0/02_UserInput.AssemblyInfo.cs
+++ CSharp_Project/02_UserInput/obj/Debug/net8.0/02_UserInput.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02_UserInput")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533f4bce493d54f22ba57ad887d7b55da124fe25")]
 [assembly: System.Reflection.AssemblyProductAttribute("02_UserInput")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02_UserInput")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
